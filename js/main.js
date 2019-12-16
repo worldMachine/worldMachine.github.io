0d83cd4aba1f6b6b90bba5fa926224542fdb5da2
SWE-Student <davi212@usc.edu>
2019-12-16 00:22:34 -0800

    Fixed responsive Navbar

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// $(window).on( 'scroll', function(){
+//     var team = $('#Education').offset().top;
+//     if ($(window).scrollTop() >= team) {
+//         // $('.cubo').css({top:"10%", left: "30%"});
+//         $('#nav-home').addClass("active");
+
+//     } else {
+//         // $('.cubo').css({top: "0", left:"0"});
+//         $('#nav-home').removeClass("active");
+//     }
+// });
+
+$( '.navbar-nav a' ).on( 'click', function () {
+	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});
+
+// if ($(window).width() < 960) {
+//     // alert('Less than 960');
+//     $('#nav-home').attr("data-toggle", "collapse");
+//     $('#nav-home').attr("data-target", "navbar-collapse");
+
+//  }
+//  else {
+//     // alert('More than 960');
+    
+//  }
+
+$(function() {
+    if($(window).width() <= 960) {
+        $(".nav-item").attr({
+          'data-toggle': 'collapse',
+          'data-target': '.navbar-collapse'
+        });
+       
+      } else {
+        $(".nav-item").removeAttr('data-toggle data-target'); 
+      }
+});
+
